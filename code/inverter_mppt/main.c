cd062048499afb1228bbd0cd42be80e1462423b2
Mark Ditsworth <markditsworth@protonmail.com>
2020-12-05 13:37:08 -0600

    add pinout diagram

--- code/inverter_mppt/main.c
+++ code/inverter_mppt/main.c
@@ -1,8 +1,66 @@
-
+/* --COPYRIGHT--,BSD
+ * Copyright (c) 2017, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * --/COPYRIGHT--*/
+/******************************************************************************
+ *
+ * Description:
+ *
+ *               MSP432P401
+ *             -----------------
+ *            |                 |
+ *            |                 |
+ *            |                 |
+ *       RST -|     P1.3/UCA0TXD|----> PC
+ *            |     P1.2/UCA0RXD|<---- PC
+ *            |                 |
+ *            |					|
+ *            |		  P2.4/TA0.1|----> PWM1
+ *            |       P2.5/TA0.2|----> PWM2
+ *            |		  P2.6/TA0.3|----> PWM3
+ *            |       P2.7/TA0.3|----> PWM4
+ *            |					|
+ *            |         P4.x/Axx|<---- ADC1
+ *            |			P4.x/Axx|<---- ADC2
+ *            |			P4.x/Axx|<---- ADC3
+ *            |         P4.x/Axx|<---- ADC4
+ *            |					|
+ *            |		   Px.x/GPIO|----> IPUT_ENABLE
+ *
+ *******************************************************************************/
 #define UART
 //#define UART_RX_INT
 #define PWM
 //#define ADC
+
+
 /* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
@@ -121,3 +179,4 @@ void TA2_N_IRQHandler(void)
            value1, value2);                         // provide values here
 }
 #endif
+
